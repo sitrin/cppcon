e59240bc47c8d4fa2bbe20ed11b231c6d3a3ecf2
EC2 Default User <ec2-user@ip-10-0-0-176.ec2.internal>
2018-09-25 21:38:22 +0000

    added cmakelist file and updated .gitignore to ignore build dir

--- src/fizzbuzz.cpp
+++ src/fizzbuzz.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
 
 int main() {
 
-   std::cout << "Hello FizzBuzz\n" ;
 
+   std::vector<std::optional<std::string>> values;
+	
+   for(size_t i=0; i<100; ++i) {
+
+
+
+   }
+
+
+   std::cout << "Size of values vector is: " << values.size() << "\n";
 }
 
