b14d4f517549ad7a6651d93571d9864889f36619
EC2 Default User <ec2-user@ip-10-0-0-176.ec2.internal>
2018-09-26 15:36:37 +0000

    switched to new project, lcg using coroutine

---
+++ src/lcg.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <boost/coroutine2/all.hpp>
+
+
+
+int main() {
+
+typedef boost::coroutines2::coroutine<int>   coro_t;
+
+coro_t::pull_type source(
+    [&](coro_t::push_type& sink){
+        int first=1,second=1;
+        sink(first);
+        sink(second);
+        for(int i=0;i<8;++i){
+            int third=first+second;
+            first=second;
+            second=third;
+            sink(third);
+        }
+    });
+
+for(auto i:source)
+    std::cout << i <<  " ";
+	
+
+
+ return 0;   
+
+}
+
